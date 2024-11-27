110dbcffce7da62f6fdb1905974cdea7c00a73d7
abbijamal <j_abbi@hotmail.com>
2024-11-27 20:54:10 +0000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   compatibilityDate: "2024-11-07"
 })
